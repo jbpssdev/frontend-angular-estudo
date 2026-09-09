44f83d7ec0bd34d4bf5337bfb6926842e86de7e9
Jackson <porccode@gmail.com>
2026-09-09 20:59:27 -0300

    Melhorando a página principal, adicionando itens e entendendo algumas coisas - além de errar o @for com @For, por causa do costume com os Decorators!

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,56 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, ViewEncapsulation } from '@angular/core';
+
+// typescript aqui!
+export interface ItemCardapio {
+  id: number;
+  nome: string;
+  descricao: string;
+  preco: number;
+  categoria: 'Bebida' | 'Petisco';
+}
 
 @Component({
-  imports: [],
   selector: 'app-root',
-  styleUrl: './app.css',
+  standalone: true,
+  imports: [],
   templateUrl: './app.html',
+  styleUrl: './app.css',
+  encapsulation: ViewEncapsulation.None
 })
 export class App {
-  protected readonly title = signal('frontAngularEstudo');
+  nomeBar = signal('Barzinho');
+
+  // fazer a lista aqui!
+  cardapio = signal<ItemCardapio[]>([
+    {
+      id: 1,
+      nome: 'Cerveja 1',
+      descricao: 'Copo de 500ml citrico',
+      preco: 10.00,
+      categoria: 'Bebida'
+    },
+    {
+      id: 2,
+      nome: 'Batata Frita',
+      descricao: 'Batata crocante e gostosa com alho!',
+      preco: 35.00,
+      categoria: 'Petisco'
+    },
+    {
+      id: 3,
+      nome: 'Hamburguer',
+      descricao: 'Blend de 180g com queijo derretido!',
+      preco: 28.50,
+      categoria: 'Petisco'
+    }
+  ]);
+
+  adicionarItem(item: ItemCardapio){
+    // interpolação!
+    alert(`Item adicionado: ${item.nome} (R$ ${item.preco.toFixed(2)})`);
+  }
+
+  fazerLogin() {
+    alert('Clicou no botão de Login!');
+  }
 }
